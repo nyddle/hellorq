c9ab7dce17cb5a8160a88116ee70214c27f04295
Alexander Davydov <root@li425-24.members.linode.com>
2013-02-05 15:15:33 +0000

    static files added

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 import time
 
+import requests
 from flask import Flask, request, session, g, redirect, url_for, abort, \
              render_template, flash, jsonify, Response
 from flask.ext.rq import RQ
@@ -8,6 +9,12 @@ from flask.ext.rq import job
 
 app = Flask(__name__)
 rq = RQ(app)
+
+
+def count_words_at_url(url):
+    resp = requests.get(url)
+    return len(resp.text.split())
+
 
 @job
 def do_something(echo):
@@ -18,6 +25,15 @@ def do_something(echo):
 @app.route('/')
 def hello_world():
     return render_template('submit.html')
+
+@app.route('/api/addjob',methods=['POST'])
+def add_job():
+    return render_template('submit.html')
+
+@app.route('/api/check_job',methods=['GET'])
+def add_job():
+    return render_template('submit.html')
+
 
 if __name__ == '__main__':
     run_simple('localhost', 8000, app, use_reloader=True, use_debugger=True, use_evalex=True)
