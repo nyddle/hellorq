0d672518fd678b603d4c4ba063d0b7e2efbd772e
Alexander Davydov <root@li425-24.members.linode.com>
2013-02-05 16:23:36 +0000

    submit added

--- hello.py
+++ hello.py
@@ -3,40 +3,32 @@ import time
 import requests
 from flask import Flask, request, session, g, redirect, url_for, abort, \
              render_template, flash, jsonify, Response
-from flask.ext.rq import RQ
+from flask_rq import RQ
 from werkzeug.serving import run_simple
-from flask.ext.rq import job
+from flask.ext.rq import job, get_queue
+
+from jobs import *
 
 app = Flask(__name__)
 rq = RQ(app)
 
-
-def count_words_at_url(url):
-    resp = requests.get(url)
-    return len(resp.text.split())
-
-
-@job
-def do_something(echo):
-    time.sleep(1)
-    return echo
-
-
 @app.route('/')
 def hello_world():
-    return render_template('submit.html')
+    return render_template('index.html')
 
 @app.route('/api/addjob',methods=['POST'])
 def add_job():
-    rq.enque(count_words_at_url, request.form['url'])
-    return 'job added'
+    q = get_queue()
+    q.enqueue(count_words_at_url, request.form['url'])
+    return render_template('index.html')
 
 @app.route('/api/check_job',methods=['GET'])
-def add_job():
-    return render_template('submit.html')
+def check_job():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
+    app.debug = True
     run_simple('localhost', 8000, app, use_reloader=True, use_debugger=True, use_evalex=True)
 
 
