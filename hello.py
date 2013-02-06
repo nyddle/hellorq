289aa5550e1ccd5dafc31e63c18be0431c1d97d9
Alexander Davydov <root@li425-24.members.linode.com>
2013-02-06 15:32:33 +0000

    sv

--- hello.py
+++ hello.py
@@ -4,15 +4,20 @@ import requests
 from flask import Flask, request, session, g, redirect, url_for, abort, \
              render_template, flash, jsonify, Response
 from flask_rq import RQ 
+from flask_rq import get_worker
 from werkzeug.serving import run_simple
 #from flask.ext.rq import job, get_queue
 import pprint
 from jobs import *
+# Creates a worker that handle jobs in ``default`` queue.
 
 app = Flask(__name__)
 app.debug = True
 
-rq = RQ(app)
+RQ(app)
+
+
+
 tasks = {}
 
 
@@ -22,20 +27,19 @@ def hello_world():
 
 @app.route('/api/addjob',methods=['POST'])
 def add_job():
-    #q = get_queue()
-    #q.enqueue(count_words_at_url, request.form['url'])
-    #print dir(job)
     #print job.result
-    tasks['task'] = count_words_at_url.delay(request.form['url'])
-    print  tasks['task']
-    return render_template('index.html')
+    print request.form['url']
+    task = count_words_at_url.delay(request.form['url'])
+    tasks[task._id] = task
+    return jsonify({'job_id' : task._id})
+
 
 @app.route('/api/check_job',methods=['GET'])
 def check_job():
     return render_template('index.html')
 
 
+
 if __name__ == '__main__':
     run_simple('localhost', 8000, app, use_reloader=True, use_debugger=True, use_evalex=True)
-
 
--- jobs.py
+++ jobs.py
@@ -1,8 +1,11 @@
 import requests
 from flask_rq import job
 
+
 @job
 def count_words_at_url(url):
-    resp = requests.get(url)
+    resp = requests.get(url) 
+    print resp.text.split()
     return len(resp.text.split())
 
+#print count_words_at_url('http://www.yandex.ru')
